d771f525a76717cdabb7b6b5aba38412110b85a7
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-07-17 10:42:27 -0300

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+
+Route::prefix('candidate')->group(function () {
+    Route::get('/login', App\Livewire\Auth\Login::class)->name('login.candidate');
+
+    Route::get('/index', App\Livewire\Candidate\Index::class)
+        ->middleware('auth')
+        ->name('index');
 });
