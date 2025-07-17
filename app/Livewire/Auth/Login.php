d887588e024502ccb45f725bab81f024315da905
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-07-16 22:05:12 -0300

    feat: create login of candidate

---
+++ app/Livewire/Auth/Login.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class Login extends Component
+{
+    #[Validate('required|email|max:255')]
+    public $email;
+
+    #[Validate('required|max:255')]
+    public $password;
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'O campo email é obrigatório.',
+            'email.email' => 'O campo email é inválido.',
+            'email.max' => 'O campo email deve ter no máximo 255 caracteres.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.max' => 'O campo senha deve ter no máximo 255 caracteres.',
+        ];
+    }
+
+    public function login()
+    {
+        $this->validate();
+
+        $credentials = [
+            'email' => $this->email,
+            'password' => $this->password,
+        ];
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('message', 'Login feito com sucesso.');
+
+            return redirect(route('index'));
+        }
+    }
+
+    public function render(): View
+    {
+        return view('livewire.auth.login');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => 'admin',
         ]);
     }
 }
