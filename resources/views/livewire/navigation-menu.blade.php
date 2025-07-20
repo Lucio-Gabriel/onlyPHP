0d4a3b845c14fa63eacdd1c05cf5057abaa20c25
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-07-20 12:21:39 -0300

    feat: create component of sidebar menu finish

--- resources/views/livewire/navigation-menu.blade.php
+++ resources/views/livewire/navigation-menu.blade.php
@@ -109,16 +109,16 @@
                     </a>
                 </li>
                 <li>
-                    <li>
-                        <button wire:click="logout"
-                            class="w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-secondary duration-300 group">
-                            <x-svg.logout
-                                class="shrink-0 w-5 h-5 text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                            <span class="ml-3 whitespace-nowrap">
-                                Sair
-                            </span>
-                        </button>
-                    </li>
+                <li>
+                    <button wire:click="logout"
+                        class="w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-secondary duration-300 group">
+                        <x-svg.logout
+                            class="shrink-0 w-5 h-5 text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                        <span class="ml-3 whitespace-nowrap">
+                            Sair
+                        </span>
+                    </button>
+                </li>
                 </li>
             </ul>
         </div>
