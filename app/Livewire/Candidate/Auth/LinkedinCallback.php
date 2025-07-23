20d154064d31a92743973cab94efd39f0af6ac39
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-07-23 16:36:49 -0300

    feat: create class component of LinkedinCallBack

---
+++ app/Livewire/Candidate/Auth/LinkedinCallback.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire\Candidate\Auth;
+
+use App\Models\User;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class LinkedinCallback extends Component
+{
+
+    public function mount()
+    {
+        $userLinkedin = Socialite::driver('linkedin')->user();
+
+        $user = User::updateOrCreate([
+            'linkedin_id' => $userLinkedin->getId(),
+        ], [
+            'name' => $userLinkedin->getName(),
+            'email' => $userLinkedin->getEmail(),
+            'avatar' => $userLinkedin->getAvatar(),
+            'linkedin_id' => $userLinkedin->getId(),
+            'linkedin_token' => $userLinkedin->token,
+            'is_candidate' => true,
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('index')->with('message', 'Login feito com sucesso com LinkedIn.');
+    }
+    public function render(): View
+    {
+        return view('livewire.candidate.auth.linkedin-callback');
+    }
+}
