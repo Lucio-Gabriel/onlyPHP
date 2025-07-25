f48b4675d18f8844beed8446eaefb3a5bb0e4419
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-07-25 12:30:23 -0300

    test(Apply-To-Vacancy): making sure that user should not apply twice at the same vacancy

--- tests/Feature/Candidate/Vacancy/ApplyToVacancyTest.php
+++ tests/Feature/Candidate/Vacancy/ApplyToVacancyTest.php
@@ -8,7 +8,7 @@ use Livewire\Livewire;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 
-it('should be able to apply to a Vacancy', function () {
+it('should be able to apply to a Vacancy', function (): void {
     $candidate = User::factory()->create();
     $vacancy = Vacancy::factory()->create();
 
@@ -18,6 +18,35 @@ it('should be able to apply to a Vacancy', function () {
         ->assertHasNoErrors()
         ->assertRedirectToRoute('index');
 
+    assertDatabaseCount('user_vacancy', 1);
+    assertDatabaseHas('user_vacancy', [
+        'vacancy_id' => $vacancy->getKey(),
+        'user_id'    => $candidate->getKey(),
+    ]);
+
+    $vacancy->refresh();
+    $applier = $vacancy->appliers()->first();
+
+    expect($applier)->toBeInstanceOf(User::class)
+        ->and($applier->id)
+        ->toBe($candidate->id)
+        ->and($applier->email)
+        ->toBe($candidate->email);
+
+});
+
+it('should not apply twice at the same vacancy', function (): void {
+    $candidate = User::factory()->create();
+    $vacancy = Vacancy::factory()->create();
+
+    for ($i = 0; $i <= 2; $i++) {
+        Livewire::actingAs($candidate)
+            ->test(ApplyToVacancy::class, ['vacancy' => $vacancy])
+            ->call('apply')
+            ->assertHasNoErrors()
+            ->assertRedirectToRoute('index');
+
+    }
     assertDatabaseCount('user_vacancy', 1);
     assertDatabaseHas('user_vacancy', [
         'vacancy_id' => $vacancy->getKey(),
