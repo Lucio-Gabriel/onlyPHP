6b46a28c7200f6bb8b672ba60a0208cb8f2a8487
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-07-26 15:55:10 -0300

    test(List-Recruiter-Vacancy): making sure that only the owner can see the vacancies

--- tests/Feature/Recruiter/Vacancy/ListVacancyTest.php
+++ tests/Feature/Recruiter/Vacancy/ListVacancyTest.php
@@ -50,3 +50,38 @@ it('should paginate vacancies that belongs to the recruiter', function () {
     $component->assertSee('Previous');
     $component->assertSee('Next');
 });
+
+test('only the owner can see the vacancies', function () {
+    $recruiter = User::factory()->recruiter()->create();
+    $wrongUser = User::factory()->recruiter()->create();
+    $vacancies = Vacancy::factory()->for($recruiter, 'owner')->count(10)->create();
+
+    $component = Livewire::actingAs($wrongUser)
+        ->test(ListVacancy::class)
+        ->assertSuccessful();
+    $vacancies->each(function (Vacancy $vacancy) use ($component) {
+        $component->assertDontSee($vacancy->title);
+        $component->assertDontSee($vacancy->stacks);
+        $component->assertDontSee($vacancy->city);
+        $component->assertDontSee(strtoupper($vacancy->state));
+        $component->assertDontSee($vacancy->company);
+        $component->assertDontSee($vacancy->contract_type);
+        $component->assertDontSee($vacancy->location);
+        $component->assertDontSee($vacancy->created_at);
+    });
+    $component->assertSee('Nenhuma Vaga Cadastrada Em Seu Perfil');
+
+    $component = Livewire::actingAs($recruiter)
+        ->test(ListVacancy::class)
+        ->assertSuccessful();
+    $vacancies->each(function (Vacancy $vacancy) use ($component) {
+        $component->assertSee($vacancy->title);
+        $component->assertSee($vacancy->stacks);
+        $component->assertSee($vacancy->city);
+        $component->assertSee(strtoupper($vacancy->state));
+        $component->assertSee($vacancy->company);
+        $component->assertSee($vacancy->contract_type);
+        $component->assertSee($vacancy->location);
+        $component->assertSee($vacancy->created_at);
+    });
+});
