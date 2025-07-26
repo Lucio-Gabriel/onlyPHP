efea20b5240bf0ad6cae1ee3ceb70e002b890930
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-07-25 22:56:36 -0300

    test(Register-Vacancy): validation tests when creating a vacancy

--- tests/Feature/Recruiter/Vacancy/CreateVacancyTest.php
+++ tests/Feature/Recruiter/Vacancy/CreateVacancyTest.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Vacancy;
 use Livewire\Livewire;
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 
@@ -52,4 +53,101 @@ it('should create a vacancy being a recruiter', function (): void {
         'location'      => 'remote',
     ]);
 
+});
+
+describe('validation tests', function () {
+    beforeEach(function () {
+        actingAs(User::factory()->recruiter()->create());
+    });
+
+    test('title::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('title', $value)
+            ->call('save')
+            ->assertHasErrors(['title' => $rule]);
+    })->with([
+        'required' => ['', 'O Título é Obrigatório.'],
+        'min:3'    => ['aa', 'O Título deve ter no mínimo 3 caracteres.'],
+        'max:255'  => [str_repeat('*', 256), 'O Título deve ter no máximo 255 caracteres.'],
+    ]);
+
+    test('description::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('description', $value)
+            ->call('save')
+            ->assertHasErrors(['description' => $rule]);
+    })->with([
+        'required' => ['', 'A Descrição é Obrigatória.'],
+        'min:3'    => ['aa', 'A Descrição deve ter no mínimo 3 caracteres.'],
+        'max:255'  => [str_repeat('*', 256), 'A Descrição deve ter no máximo 255 caracteres.'],
+    ]);
+
+    test('city::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('city', $value)
+            ->call('save')
+            ->assertHasErrors(['city' => $rule]);
+    })->with([
+        'required' => ['', 'A Cidade é Obrigatória.'],
+        'min:3'    => ['aa', 'A Cidade deve ter no mínimo 3 caracteres.'],
+        'max:100'  => [str_repeat('*', 101), 'A Cidade deve ter no máximo 100 caracteres.'],
+    ]);
+
+    test('company::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('company', $value)
+            ->call('save')
+            ->assertHasErrors(['company' => $rule]);
+    })->with([
+        'required' => ['', 'A Empresa é Obrigatória.'],
+        'min:3'    => ['aa', 'A Empresa deve ter no mínimo 3 caracteres.'],
+        'max:255'  => [str_repeat('*', 256), 'A Empresa deve ter no máximo 255 caracteres.'],
+    ]);
+
+    test('stacks::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('stacks', $value)
+            ->call('save')
+            ->assertHasErrors(['stacks' => $rule]);
+    })->with([
+        'min:3'   => ['aa', 'As Stacks devem ter no mínimo 3 caracteres.'],
+        'max:255' => [str_repeat('*', 256), 'As Stacks devem ter no máximo 255 caracteres.'],
+    ]);
+
+    test('salary::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('salary', $value)
+            ->call('save')
+            ->assertHasErrors(['salary' => $rule]);
+    })->with([
+        'required' => ['', 'O Salário é Obrigatório.'],
+        'numeric'  => ['ahhahahah', 'Apenas números são permitidos no campo de salário'],
+    ]);
+
+    test('type::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('type', $value)
+            ->call('save')
+            ->assertHasErrors(['type' => $rule]);
+    })->with([
+        'required' => ['', 'O Tipo de Vaga é Obrigatório.'],
+    ]);
+
+    test('contract_type::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('contract_type', $value)
+            ->call('save')
+            ->assertHasErrors(['contract_type' => $rule]);
+    })->with([
+        'required' => ['', 'O Tipo de Contrato é Obrigatório.'],
+    ]);
+
+    test('location::validation', function ($value, $rule): void {
+        Livewire::test(CreateVacancy::class)
+            ->set('location', $value)
+            ->call('save')
+            ->assertHasErrors(['location' => $rule]);
+    })->with([
+        'required' => ['', 'A Modalidade de Trabalho é Obrigatória.'],
+    ]);
 });
